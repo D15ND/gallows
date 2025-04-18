92c176e768b6efbc519fec6b487da567c10a5aa1
ILYA <106765406+D15ND@users.noreply.github.com>
2025-04-18 23:38:44 +0300

    feat: add new rulles

--- index.js
+++ index.js
@@ -16,10 +16,13 @@ for (let i = 0; i < word.length; i++) {
 }
 
 let remainingLetters = word.length;
+let trySum=0;
+let tryLimmit=20;
 
 while (remainingLetters > 0) {
   alert(answerArray.join(" "));
   let guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры");
+  trySum++;
   guess=guess.toLowerCase();
   if (guess === null) {
     // alert("Игра окончена не начавшись")
@@ -27,6 +30,7 @@ while (remainingLetters > 0) {
   } else if (guess.length !== 1) {
     alert("Введите одну букву");
   } else {
+    tryLimmit--;
     for (let j = 0; j < word.length; j++) {
       if (word[j] === guess) {
         answerArray[j] = guess;
@@ -37,4 +41,11 @@ while (remainingLetters > 0) {
 }
 
 alert(answerArray.join(" "));
-alert("Отгадано, было слово: " + word);
+
+if (tryLimmit>0) {
+  alert("Отгадано, было слово: " + word);
+  alert("Количество попыток: "+trySum);
+} else {
+  alert("Попытки кончились, ты проиграл. Слово было: "+word);
+  alert("Твои попытки: "+trySum);
+}
